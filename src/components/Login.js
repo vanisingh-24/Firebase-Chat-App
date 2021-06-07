9b331bc4e73142730f2779ffe4829a40d8bdddab
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-06-07 14:52:12 +0530

    context

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import "firebase/app";
+
+import { auth } from '../firebase';
 
 const Login = () => {
    return (
@@ -7,13 +10,13 @@ const Login = () => {
           <div id="login-card">
               <h2> Welcome to Unichat! </h2>
 
-              <div className="login-button google">
+              <div className="login-button google" onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                   <GoogleOutlined /> Sign In with Google
               </div>
 
               <br/> <br/>
 
-              <div className="login-button facebook">
+              <div className="login-button facebook" onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
                   <FacebookOutlined /> Sign In with Facebook
               </div>
           </div>
